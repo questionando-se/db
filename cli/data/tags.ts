ee5289f13ac53970cdb33b18401b44d9a1a155d6
EduardoJM <eduardo_y05@outlook.com>
2020-09-12 20:59:25 -0300

    adicionada tag 'teorica'

--- cli/data/tags.ts
+++ cli/data/tags.ts
@@ -2,6 +2,9 @@
  * Tag id and display text.
  */
 const tags: { [key: string]: string } = {
+    // common
+    "teorica": "Teórica",
+    // math
     "geometria-plana": "Geometria Plana",
     "geometria-analitica": "Geometria Analítica",
     "geometria-espacial": "Geometria Espacial",
